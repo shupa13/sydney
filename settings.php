60e828b57ef8a7899ea0dc6f2a3515fc461b838d
shupa13 <shupa813@gmail.com>
2018-11-22 12:22:21 +0900

    threemid done

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+  $settings = array(
+    "clientID" =>"dbfdc2aae60d47a083abb536c80fb10f",
+    "clientSecret" => "5defbb18dd1d4e86a3a2221bba8109c1",
+    "redirectURI" => "http://localhost:81/wordpress"
+  );
+ ?>
